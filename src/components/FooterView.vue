d8e55d8c9a78359e53108d0e2a121dbf3d7397f1
windihotspot <boiwindihotspot@gmail.com>
2025-11-24 09:52:24 +0100

    location

--- src/components/FooterView.vue
+++ src/components/FooterView.vue
@@ -1,31 +1,39 @@
 <template>
-  <footer class="bg-gradient-to-r from-red-100 via-red-200 to-red-100 text-black py-12 mx-auto text-center">
+  <footer
+    class="bg-gradient-to-r from-red-100 via-red-200 to-red-100 text-black py-12 mx-auto text-center"
+  >
     <div class="max-w-7xl mx-auto px-6 text-center">
-
       <!-- Middle Info -->
       <div class="mx-auto text-center pt-2">
-
         <!-- Logo and description -->
         <div>
           <div class="flex items-center justify-center gap-2 mb-3">
             <img src="../assets/glo-kitchen.jpg" alt="Glo kitchen" class="w-10 h-10 rounded-full" />
             <div>
-              <span class="font-semibold">Glo Kitchen</span>
-               <div class="font-light text-black text-sm">Intercontinental takeaway foods</div>
+              <span class="font-semibold text-start">Glo Kitchen</span>
+              <div class="font-light text-black text-sm">Intercontinental takeaway foods</div>
             </div>
-            
           </div>
         </div>
-
       </div>
 
       <!-- Bottom copyright -->
-      <div class="pt-2 text-gray-500 text-xs text-center">
-        © 2025 Glo Kitchen. All rights reserved.
-        
+      <div class="pt-2 text-gray-500 text-xs text-center flex flex-col items-center space-y-1">
+        <!-- Icon -->
+        <img
+          src="../assets/icons8-location-50.png"
+          alt="location icon"
+          class="w-5 h-5 mx-auto opacity-70"
+        />
+
+        <!-- Address -->
+        <div class="mb-4 font-semibold">
+          37 Ballymahon street town parks, @ pig market lane Longford  Ireland
+        </div>
+
+        <!-- Footer text -->
+        <div>© 2025 Glo Kitchen. All rights reserved.</div>
       </div>
     </div>
   </footer>
-
-  
 </template>
